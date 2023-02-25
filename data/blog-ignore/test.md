a91d225cea5d610d491df32df4c7a37309a24899
matt penrice <matthew.penrice@gmail.com>
2023-02-25 16:54:08 +0100

    first commit

---
+++ data/blog-ignore/test.md
@@ -0,0 +1,9 @@
+---
+title: test
+date: '2023-02-25'
+tags: ['dev']
+draft: true
+summary: another-cautionary-just-ship-it-tale
+images: []
+layout: PostLayout
+---
---
+++ data/projectsData.ts
@@ -0,0 +1,10 @@
+const projectsData = [
+  {
+    title: 'TBD',
+    description: `Business as Code (BaC) is the encoding of business knowledge within an org's software. This knowledge, whilst often overlooked, offers great insight into an org's real performance.`,
+    imgSrc: '/static/images/social-network.jpg',
+    href: 'https://www.google.com',
+  },
+]
+
+export default projectsData
